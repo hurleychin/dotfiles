fe48cce9d0ff2b8f667081b43921ffb2370508de
hurleychin <qinhulin@gmail.com>
2023-02-15 23:29:54 +0800

    modified:   .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    neofetch
 end
 
-# User configuration
-set -U fish_greeting
+## User configuration
+set fish_greeting
 set -gx LANG en_US.UTF-8
 set -gx EDITOR nvim
 
-# ls, the common ones I use a lot shortened for rapid fire usage
+## Useful aliases
 alias l='ls -lFh'     #size,show type,human readable
 alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
@@ -20,9 +21,9 @@ alias lrt='ls -1Fcrt'
 alias lsr='ls -lARFh' #Recursive list of files and directories
 alias lsn='ls -1'     #A column contains name of files and directories
 
+alias setproxy="set -gx http_proxy http://rickgo.cf:9001 && set -gx https_proxy http://rickgo.cf:9001 && set -gx ALL_PROXY socks5://rickgo.cf:9000"
+alias unsetproxy="set -e http_proxy && set -e https_proxy && set -e ALL_PROXY"
 alias cht="cht.sh"
 alias m="micro"
 alias vim="nvim"
 alias pupu="pkg update&&pkg upgrade"
-alias setproxy="set -gx http_proxy http://rickgo.cf:9001 && set -gx https_proxy http://rickgo.cf:9001 && set -gx ALL_PROXY socks5://rickgo.cf:9000"
-alias unsetproxy="set -e http_proxy && set -e https_proxy && set -e ALL_PROXY"
